1ffe35d29646359a12b4a6e39f65c0f5484d0d7b
Stig Kukec <skukec@racunarstvo.hr>
2023-08-18 12:58:05 +0200

    Nadogradnja forme prijava, dodana klasa za rad sa putanjama datoteka.

---
+++ Utilities/FilePaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class FilePaths
+    {
+        private static readonly string direktorij = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).Parent.FullName;
+
+        public static string GetFilePath(string filePath)
+        {
+            string accountPath = Path.Join(direktorij, $@"{filePath}");
+            return accountPath;
+        }
+    }
+}
